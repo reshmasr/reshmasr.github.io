651a64d37494c056819a2c0ba3530a86ac2169b7
Reshma S Raveendran <reshmasraveendran@gmail.com>
2020-05-11 23:38:12 +0530

    Text styling

--- D3Charts/transformation-map.js
+++ D3Charts/transformation-map.js
@@ -69,8 +69,9 @@ async function renderChart() {
 
     const nodeCircle = node
         .append('circle')
+        .attr('class', 'node-circle')
         .attr('r', 10)
-        .style('fill', '#fff')
+        .attr('fill', '#fff')
         .attr('cursor', 'pointer')
         .style('display', (d) =>
             typeof d.id === typeof '' ? 'static' : 'none'
@@ -86,25 +87,19 @@ async function renderChart() {
             'd',
             'M-5.5,-8.5 h6 q6,0 6,6 v10 q0,1 -1,1 h-11 q-1,0 -1,-1 v-15 q0,-1 1,-1 Z'
         )
-        .style('display', (d) => (d.children ? 'none' : 'static'));
+        .style('display', (d) =>
+            typeof d.id === typeof '' ? 'none' : 'static'
+        );
 
-    nodeCircle
-        .on('mouseenter', function (d) {
-            is_connected(d, 0.1, link, nodePath, data);
-            d3.select(this)
-                .transition()
-                .duration(100)
-                .attr('r', 20)
-                .attr('fill', '#437def');
-        })
-        .on('mouseleave', function (d) {
-            d3.select(this)
-                .transition()
-                .duration(100)
-                .attr('r', 10)
-                .attr('fill', '#fff');
-            is_connected(d, 1, link, nodePath, data);
-        });
+    nodeCircle.on('mouseenter', function (d) {
+        svg.selectAll('.node-circle').attr('fill', '#fff').attr('r', 10);
+        d3.select(this)
+            .transition()
+            .duration(100)
+            .attr('r', 20)
+            .attr('fill', '#437def');
+        is_connected(d, link, nodePath, data);
+    });
 
     // Let's list the force we wanna apply on the network
     var simulation = d3
@@ -121,27 +116,25 @@ async function renderChart() {
         .force('charge', d3.forceManyBody().strength(-10)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
         .on('tick', ticked);
 
-    node.append('text')
-        .attr('dy', '0.31em')
-        .attr('x', 10)
-        // .attr('text-anchor', (d) =>
-        //     typeof d.id === typeof ''
-        //         ? 'start'
-        //         : d.x < 0 && d.y < 0
-        //         ? 'end'
-        //         : 'start'
-        // )
-        .style('margin', '10px')
-        // .attr('class', 'text-indicator')
-        // .style('transform', (d) =>
-        //     typeof d.id !== typeof '' ? 'rotate(-45deg)' : null
-        // )
-        .text((d, i) => {
-            console.log(d.x, d.name);
-            return 'TVM';
-        })
-        .clone(true)
-        .lower();
+    setTimeout(() => {
+        node.append('text')
+            .attr('class', 'text-indicator')
+            .attr('x', (d) => (d.x < Math.PI ? -20 : 20))
+            .attr('text-anchor', (d) => {
+                return d.x < Math.PI ? 'end' : 'start';
+            })
+            .attr('transform', (d) => {
+                return typeof d.id !== typeof ''
+                    ? `rotate(${(Math.atan(d.y / d.x) * 180) / 3.14})`
+                    : 'rotate(0)';
+            })
+            .text((d, i) => {
+                return 'TVM';
+                // return d.name;
+            })
+            .clone(true)
+            .lower();
+    }, 500);
 
     // This function is run at each iteration of the force algorithm, updating the nodes position.
     function ticked() {
@@ -241,6 +234,7 @@ async function renderChart() {
                 } else if (d.source.y < 0) {
                     return renderFour();
                 } else {
+                    // console.log(d.source.x, d.source.y);
                     return renderTwo(); //N1, N2, N3
                 }
             } else {
@@ -265,19 +259,16 @@ function autosize(svg) {
     svg.setAttribute('viewBox', `${box.x} ${box.y} ${box.width} ${box.height}`);
     return svg;
 }
-function is_connected(d, opacity, lines, nodes, data) {
-    // data.links.find()
-    lines
-        .transition()
-        .style('stroke-opacity', function (o) {
-            return o.source === d || o.target === d ? 1 : opacity;
-        })
-        .attr('stroke', function (o) {
-            return o.source === d || o.target === d ? '#437def' : '#fff';
-        });
-
+function is_connected(d, lines, nodes, data) {
+    const endNodes = {};
+    lines.transition().attr('stroke', function (o) {
+        if (o.source === d) {
+            endNodes[o.target.id] = o.target;
+        }
+        return o.source === d || o.target === d ? '#437def' : '#fff';
+    });
     nodes.transition().attr('fill', function (o) {
-        return o.parent === d ? '#437def' : '#fff';
+        return endNodes[o.id] === o ? '#437def' : '#fff';
     });
 }
 $(document).ready(function () {
